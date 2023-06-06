d4c3068a52145af57e348d2ef36b92650af94a8f
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-06 22:49:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+hatedChar = input("Which is the character you hate the most? ")
+funnyName = input("Which is the weirdest name you know? ")
+presidentPet = input("If you could pet a president, how you would name it? ")
+print("Three brothers where hanging out in a city of guns, love and sex. They were magicians, descendants of the first among all of them. One day, while they were smoking marihuana with LSD in the middle of a forest, after getting lost and losing even the wands -wich they never needed anyway-, a great genious arrived and told them to ask three whishes. After this,", "\033[31m", "the genious, drunk and stumbling, mambled some words and pointed it's finger to the three scared wizards.", "\033[0m", "The first of them became", hatedChar,". The second, the oldest one, became", funnyName, ". Lastly, the third was called (Yes!, you guess it!) Petsident", presidentPet)
